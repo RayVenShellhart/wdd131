59223cc53faea5d5650652a2629b351f86bd97dd
RayVenShellhart <rayvenshellhart@gmail.com>
2025-03-13 17:39:43 -0700

    temple

---
+++ scripts/temples.js
@@ -0,0 +1,31 @@
+// last modified
+
+const year = document.querySelector("#year")
+
+const today = new Date();
+const short = document.querySelector("#short");
+
+currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+let oLastModif = new Date(document.lastModified);
+
+let nLastModif = Date.parse(document.lastModified);
+
+lastModified.innerHTML = `<p id="lastModified">Last Modified: ${new Intl.DateTimeFormat("en-US", {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+}).format(nLastModif)}</p>`;
+
+// hamburger button
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
