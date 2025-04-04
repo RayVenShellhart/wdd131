ca7547cd184b5f0638e61c8eb6db762a98117ee4
RayVenShellhart <rayvenshellhart@gmail.com>
2025-04-04 15:11:14 -0700

    fixed-review-count

--- scripts/review.js
+++ scripts/review.js
@@ -4,9 +4,9 @@
 const visitsDisplay = document.querySelector(".visits");
 
 
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 1;
 
-if (numVisits !== 0) {
+if (numVisits !== 1) {
     visitsDisplay.textContent = numVisits;
 } else {
     visitsDisplay.textContent = `This is your first review. YAY!`;
