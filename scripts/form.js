f6051cf50b99417272aab352cdc17c5046f5d232
RayVenShellhart <rayvenshellhart@gmail.com>
2025-04-04 15:04:02 -0700

    form

---
+++ scripts/form.js
@@ -0,0 +1,66 @@
+
+
+
+// basic footer content
+
+const year = document.querySelector("#year")
+
+const today = new Date();
+const short = document.querySelector("#short");
+
+currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+let oLastModif = new Date(document.lastModified);
+
+let nLastModif = Date.parse(document.lastModified);
+
+lastModified.innerHTML = `<p id="lastModified">Last Modified: ${new Intl.DateTimeFormat("en-US", {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+}).format(nLastModif)}</p>`;
+
+
+// form select
+dropdownElement = document.getElementById("product-selection")
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+products.forEach(({ name, id }) =>
+    {
+    dropdownElement.add(new Option(name, id))
+});
+dropdownElement.onchange = () => {
+    console.log(dropdownElement.value)
+};
+
+
+
---
+++ scripts/review.js
@@ -0,0 +1,39 @@
+// form.js was clashing with the html so I had to give review.html its own js.
+// review confirmatiom
+
+const visitsDisplay = document.querySelector(".visits");
+
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `This is your first review. YAY!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
+// basic footer content
+
+const year = document.querySelector("#year")
+
+const today = new Date();
+const short = document.querySelector("#short");
+
+currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+let oLastModif = new Date(document.lastModified);
+
+let nLastModif = Date.parse(document.lastModified);
+
+lastModified.innerHTML = `<p id="lastModified">Last Modified: ${new Intl.DateTimeFormat("en-US", {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+}).format(nLastModif)}</p>`;
